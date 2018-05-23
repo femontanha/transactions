dde3d9bda40d7a4291670fe99a0c97f435248699
femontanha <femontanhanew@gmail.com>
2018-05-23 19:09:41 -0300

    Setup css variables and replace them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import TransactionContainer from './containers/Transaction/TransactionContainer';
-import './App.css';
 
 class App extends Component {
   render() {
